767aeecdb47f2075eb98e7664a2d6adb9cff27c3
DF T480 <fedejko@gmail.com>
2021-10-07 20:29:46 +0200

    adjust bar height

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int user_bh            = 50;
+static const int user_bh            = 32;
 static const char *fonts[]          = { "IBMPlexMono-Regular:matrix:pixelsize=14:antialias=true:autohint=true" };
 static const char dmenufont[]       =   "IBMPlexMono-Regular:matrix:pixelsize=14:antialias=true:autohint=true";
 static const char col_gray1[]       = "#000000";
